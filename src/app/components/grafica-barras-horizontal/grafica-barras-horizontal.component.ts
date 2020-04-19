ffc58a037c335bf31b11c1911b358b766b0d2a47
Alejandro <el_grande_ahc@hotmail.com>
2020-04-19 07:44:32 -0500

    Creacion y configuracion de paginas y componentes iniciales

---
+++ src/app/components/grafica-barras-horizontal/grafica-barras-horizontal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grafica-barras-horizontal',
+  templateUrl: './grafica-barras-horizontal.component.html',
+  styles: []
+})
+export class GraficaBarrasHorizontalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
